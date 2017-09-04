62c80697d9e8c4a5f5b57832e3930313732836c4
Jay McCarthy <jay.mccarthy@gmail.com>
2017-09-04 15:25:31 +0100

    Add deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,7 @@
 #lang info
 (define collection 'multi)
 (define deps '("base" "rackunit-lib"))
-(define build-deps '())
+(define build-deps '("racket-doc"
+                     "rackunit-doc"
+                     "scribble-lib"
+                     ))
